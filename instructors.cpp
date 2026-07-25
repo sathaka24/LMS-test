65777034de5724c606c5550a248c5cac0d9bd69c
abiiya <isurangaabishek0@gmail.com>
2026-07-26 00:48:50 +0530

    done

--- instructors.cpp
+++ instructors.cpp
@@ -1,3 +1,10 @@
 class instructors{
 
 }
+class instructors{
+    public:
+        void addInstructor();
+        void removeInstructor();
+        void updateInstructor();
+        void listInstructors();
+};
